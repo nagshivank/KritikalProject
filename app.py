8f8a862ea1bdfea77caacbaa1032a9ea5ff421b9
Shivank Nag <60806159+nagshivank@users.noreply.github.com>
2024-08-10 18:04:21 -0700

    Adding Scripts for the API Interface and Requirements

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, request, jsonify
-from folderparser import parse_repository, extract_functions_and_imports, recommend_function
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/search', methods=['POST'])
 def search():
     data = request.json
     query = data.get('query')
     
-    dir_path = r'~/Projects/'
+    dir_path = r'Projects/'
     python_files = parse_repository(dir_path)
     
     functions_dict = extract_functions_and_imports(python_files)
